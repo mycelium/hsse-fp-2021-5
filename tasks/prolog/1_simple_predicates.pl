e6418f0345730e8f9b150ca333563e1965420d9b
Dmitrii Vikhliaev <dim49v@yandex.ru>
2021-09-17 23:42:51 +0300

    lab 1 prolog

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -10,8 +10,48 @@
 	father(b,d).  % 3
 	father(b,e).  % 4
 	father(c,f).  % 5
+
+	%1
+	brother(X, Y) :- father(Z, X), father(Z, Y), Y \= X.
+	%2
+	cousin(X, Y) :- father(Z, X), father(W, Y), brother(Z, W).
+	%3
+	grandson(X, Y) :- father(W, X), father(Y, W).
+	%4
+	descendent(X, Y) :- (father(Y, X); grandson(X, Y)).
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+	% ?- brother(X,Y).
+	% ?- cousin(X,Y).
+	% ?- grandson(X,Y).
+	% ?- descendent(X,Y).
+	query() :- write("brothers"), nl, brother(X, Y), write(X), write(" "), write(Y), nl, fail;
+		write("cousins"), nl, cousin(X, Y), write(X), write(" "), write(Y), nl, fail;
+		write("grandsons"), nl, grandson(X, Y), write(X), write(" "), write(Y), nl, fail;
+		write("descendants"), nl, descendent(X, Y), write(X), write(" "), write(Y), nl, fail.
+/*
+	?- query().
+	brothers
+	b c
+	c b
+	d e
+	e d
+	cousins
+	d f
+	e f
+	f d
+	f e
+	grandsons
+	d a
+	e a
+	f a
+	descendants
+	b a
+	c a
+	d b
+	e b
+	f c
+	d a
+	e a
+	f a
+	false.
+*/

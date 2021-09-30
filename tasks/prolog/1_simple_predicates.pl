720bb9117a740586837b2ef573ec615c965c13ac
Sophia <sofiya_shalgueva@mail.ru>
2021-09-30 21:51:07 +0300

    Add prolog Lab 1

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -10,8 +10,34 @@
 	father(b,d).  % 3
 	father(b,e).  % 4
 	father(c,f).  % 5
+	
+brother(X, Y) :- father(Z, X), father(Z, Y), not(X = Y).
+cousin(X, Y) :- father(Z, X), father(P, Y), brother(Z, P).
+grandson(X, Y) :- father(Z, X), father(Y, Z).
+descendent(X, Y) :- father(Y, X) ; (father(Z, X), descendent(Z, Y)).
+
+
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+?- brother(X,Y).
+% X = b, Y = c ;
+% X = c, Y = b ;
+% X = d, Y = e ;
+% X = e, Y = d ;
+?- cousin(X,Y).
+% X = d, Y = f ;
+% X = e, Y = f ;
+% X = f, Y = d ;
+% X = f, Y = e ;
+?- grandson(X,Y).
+% X = d, Y = a ;
+% X = e, Y = a ;
+% X = f, Y = a.
+?- descendent(X,Y).
+% X = b, Y = a ;
+% X = c, Y = a ;
+% X = d, Y = b ;
+% X = e, Y = b ;
+% X = f, Y = c ;
+% X = d, Y = a ;
+% X = e, Y = a ;
+% X = f, Y = a ;

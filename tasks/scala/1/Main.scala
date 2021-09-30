16ff0934663db46f9bcc01cc3267211724106232
Sophia <sofiya_shalgueva@mail.ru>
2021-09-30 22:00:24 +0300

    Add Scala Lab 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,4 @@
 package recfun
-import common._
 
 object Main {
   def main(args: Array[String]) {
@@ -15,14 +14,23 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    assert(c <= r, "Column shouldn't be grater than row")
+    if (c == 0 || r == c) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def countParentheses(chars: List[Char], acc: Int = 0): Boolean = {
+      if (chars.isEmpty) acc == 0
+      else if ("(".contains(chars.head)) countParentheses(chars.tail, acc + 1)
+      else if (")".contains(chars.head)) acc > 0 && countParentheses(chars.tail, acc - 1)
+      else countParentheses(chars.tail, acc)
+    }
+
+    countParentheses(chars)
   }
 
   /**
@@ -33,6 +41,12 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    def countCombinations(money: Int, coins: List[Int], currentSum: Int = 0): Int = {
+      if (money == currentSum) 1
+      else if (currentSum > money || coins.isEmpty) 0
+      else countCombinations(money, coins, currentSum + coins.head) + countCombinations(money, coins.tail, currentSum)
+    }
 
+    countCombinations(money, coins)
   }
 }

af96541240488f1ce8ddea70eb94158e85aae49a
Даниил <afanasyev.dp@gmail.com>
2021-10-12 13:56:41 +0300

    Upload task 2 prolog

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,21 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A,B) :- A, B.
+
+or(A,_) :- A,!.
+or(_,B) :- B.
+
+xor(A,B) :- not(equ(A,B)).
+
+equ(A,B) :- or(and(A,B), and(not(A),not(B))).
+
+bind(true).
+bind(fail).
+
+truth_table(A,B,Expr) :- bind(A), bind(B), do(A,B,Expr), fail.
+
+do(A,B,_) :- write(A), write('  '), write(B), write('  '), fail.
+do(_,_,Expr) :- Expr, !, write(true),nl.
+do(_,_,_) :- write(fail), !,nl.

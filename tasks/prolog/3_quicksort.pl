cca6cbd621cdcddccc876f362114528218c81f68
Даниил <afanasyev.dp@gmail.com>
2021-10-12 13:57:48 +0300

    Upload task 3 prolog

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,18 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% L - in array, K - out array
+
+qsort([X|Xs],Ys) :-
+  partition(Xs,X,Left,Right),
+  qsort(Left,Ls),
+  qsort(Right,Rs),
+  append(Ls,[X|Rs],Ys), !.
+qsort([],[]).
+
+partition([X|Xs],Y,[X|Ls],Rs) :-
+  X =< Y, partition(Xs,Y,Ls,Rs).
+partition([X|Xs],Y,Ls,[X|Rs]) :-
+  X > Y, partition(Xs,Y,Ls,Rs).
+partition([],_,[],[]).
+
+append([],Ys,Ys).
+append([X|Xs],Ys,[X|Zs]) :- append(Xs,Ys,Zs).

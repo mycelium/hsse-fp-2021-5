b2ce363f61864566820621d61b58842d12290ab9
Zhukova Ksenia <zhukova.ksenia2000@gmail.com>
2021-10-20 18:54:18 +0300

    Add Prolog task 3

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,25 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K)
+% который для заданного списка целых чисел возвращает отсортированный
+
+qsort(L, K) :-
+    L = [Pivot|_], %choose pivot
+    split_list(L, Pivot, Left, PivotList, Right),
+    qsort(Left, Lsorted),
+    qsort(Right, Hsorted),
+    append(Lsorted, PivotList, LeftPart),
+    append(LeftPart, Hsorted, K).
+qsort([],[]).
+
+% args: List, pivot elem, 3 target lists
+split_list([Head|Tail], Pivot, Left, PivotList, Right) :-
+    Head < Pivot,
+    split_list(Tail, Pivot, Nl, PivotList, Right),
+    append(Nl, [Head], Left).
+split_list([Head|Tail], Pivot, Left, PivotList, Right) :-
+    Head > Pivot,
+    split_list(Tail, Pivot, Left, PivotList, Nh),
+    append(Nh, [Head], Right).
+split_list([Head|Tail], Pivot, Left, PivotList, Right) :-
+    split_list(Tail, Pivot, Left, Npl, Right),
+    append(Npl, [Head], PivotList).
+split_list([],_,[],[],[]).

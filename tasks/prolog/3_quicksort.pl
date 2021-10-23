90dc7f5d5c0a69ec6a5c4fb299884716bb6e401c
Shapovalova Irina <irishapovalova13@gmail.com>
2021-10-23 20:15:54 +0300

    Prolog lab 3

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,15 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+
+qsort([], []).
+qsort([Head|Tail], K) :- 
+	partition(Head, Tail, Less, More),
+	qsort(Less, SortedLess),
+	qsort(More, SortedMore),
+	append([SortedLess, [Head], SortedMore], K).
+	
+% Разбиение - делим список на две группы относительно опорного элемента
+% Если элемент H меньше или равен опорному Pivot, то он идет в список Less
+% Если элемент H больше опорного Pivot, то он идет в список More
+partition(Pivot, [Head|Tail], [Head|Less], More) :- Head =< Pivot, partition(Pivot, Tail, Less, More).
+partition(Pivot, [Head|Tail], Less, [Head|More]) :- Head > Pivot,  partition(Pivot, Tail, Less, More).
+partition(_, [], [], []).

376aa67cac507295c28b9f223c9a83555868b658
dumaevrinat <dumaevrinat@gmail.com>
2021-10-24 18:56:00 +0300

    add prolog - 3

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,17 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+qsort([], []).
+
+qsort([H|T], Sorted) :-
+    split(H, T, Less, Greater),
+    qsort(Less, SortedLess),
+    qsort(Greater, SortedGreater),
+    append(SortedLess, [H|SortedGreater], Sorted).
+
+split(_, [], [], []).
+
+split(P, [H|T], Less, [H|Greater]) :- 
+    H > P, split(P, T, Less, Greater).
+
+split(P, [H|T], [H|Less], Greater) :- 
+    H =< P, split(P, T, Less, Greater). 

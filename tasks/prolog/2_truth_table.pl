5efe45f5ff6c5f37fd2de8184434aa956399a6eb
tkachuk andrey <tka4uk.andrei@yandex.ru>
2021-10-31 20:46:20 +0300

    task 2 complete

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,23 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A, B) :- A, B.
+or(A, B) :- A; B.
+xor(A, B) :- not(A), B; A, not(B).
+% not(A) wat??? It's already defined!
+equ(A, B) :- A = B.
+
+bool(true).
+bool(fail).
+
+truth_table(A, B, Expression) :-
+	bool(A),
+	bool(B),
+	write(A),
+	write(" "),
+	write(B),
+	write(" "),
+	(Expression -> write("true"); write("fail")),
+	writeln(""),
+	false. % не даём вывести лишнюю информацию (а именно на каких значениях идёт перебор). false можно заменить на fail

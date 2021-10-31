c3b1655570094296b8950c4909066728a60bf687
Shapovalova Irina <irishapovalova13@gmail.com>
2021-10-31 15:22:41 +0300

    Haskell lab 1

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,10 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys = if length xs == length ys 
+                 then Just (calculateDistance xs ys)
+                 else Nothing
+
+calculateDistance :: String -> String -> Int
+calculateDistance (xh:xtail) (yh:ytail) = fromEnum(xh /= yh) + calculateDistance xtail ytail
+calculateDistance _ _ = 0

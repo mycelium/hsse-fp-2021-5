abde2b9afcdb3789165c31fe79990f17a6282118
Zhukova Ksenia <zhukova.ksenia2000@gmail.com>
2021-11-02 00:33:20 +0300

    Fix indentations in Prolog task 4

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -30,18 +30,18 @@ halve(List, L, R) :-
 %
 % С табами выглядело бы так:
 % Tree = instant(5,
-% 			   instant(2,
-% 			   		   instant(1, instant(0, empty, empty), empty),
-% 			   		   instant(4, instant(3, empty, empty), empty)
-% 			   		   ),
-% 			   instant(7,
-% 			   		   instant(7, instant(6, empty, empty), empty),
-% 			   		   instant(10, instant(9, empty, empty), empty)
-% 			   		   )
-% 			   )
+%                instant(2,
+%                        instant(1, instant(0, empty, empty), empty),
+%                        instant(4, instant(3, empty, empty), empty)
+%                        ),
+%                instant(7,
+%                        instant(7, instant(6, empty, empty), empty),
+%                        instant(10, instant(9, empty, empty), empty)
+%                        )
+%                )
 % 
 % Само дерево:
-% 				5
-% 		2				7
-% 	1		4		7		10
-% 0	 --   3  --   6  --   9   --
+%               5
+%       2               7
+%   1      4       7        10
+% 0  --   3  --   6  --   9   --

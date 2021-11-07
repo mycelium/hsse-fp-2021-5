219368e5617eed064b6bf025992d25bf5a4f6d92
Kristijan271197 <59137001+Kristijan271197@users.noreply.github.com>
2021-11-07 19:34:59 +0300

    Scala Lab 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -14,15 +14,29 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
+  def pascal(column: Int, row: Int): Int = {
+    if (column < 0 || row < 0) {
+      throw new IllegalArgumentException("Column and row must be positive.")
+    } else if (column > row) {
+      throw new IllegalArgumentException("Row number must be greater than column.")
+    }
 
+    if (column == 0 || column == row) 1
+    else pascal(column - 1, row - 1) + pascal(column, row - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
-  def balance(chars: List[Char]): Boolean = {
-   
+  def balance(expressionChars: List[Char]): Boolean = {
+    def indexParentheses(chars: List[Char], parenthesesBalance: Int): Int = {
+      if (chars.isEmpty || parenthesesBalance < 0) parenthesesBalance
+      else if (chars.head == ')') indexParentheses(chars.tail, parenthesesBalance - 1)
+      else if (chars.head == '(') indexParentheses(chars.tail, parenthesesBalance + 1)
+      else indexParentheses(chars.tail, parenthesesBalance)
+    }
+
+    indexParentheses(expressionChars, 0) == 0
   }
 
   /**
@@ -33,6 +47,12 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    def change(money: Int, coins: List[Int]): Int = {
+      if (money < 0 || coins.isEmpty) 0
+      else if (money == 0) 1
+      else change(money, coins.tail) + change(money - coins.head, coins)
+    }
 
+    change(money, coins)
   }
 }

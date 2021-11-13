e484b14a22ed382c21341eddc364f677ec6c3b77
Keler1024 <sergeydrozdov.1999@gmail.com>
2021-11-13 14:50:44 +0300

    Prolog task 2 done

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,26 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A, B) :- A, B.
+or(A, _) :- A.
+or(_, B) :- B.
+xor(A, B) :- A, not(B).
+xor(A, B) :- not(A), B.
+equ(A, B) :- and(A, B).
+equ(A, B) :- and(not(A), not(B)).
+
+boolean(true).
+boolean(fail).
+
+truth_table(A, B, Expression) :- boolean(A), boolean(B), 
+								(write(A), write(" "), write(B), write(" ")),
+								(Expression -> write("true"); write("fail")),
+								nl,
+			                    fail.
+
+?- truth_table(A,B,xor(A,or(A,B))).
+% true true fail
+% true fail fail
+% fail true true
+% fail fail fail

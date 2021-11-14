5609219846a088202020aa5ef1c7827d43bdf1ff
Keler1024 <sergeydrozdov.1999@gmail.com>
2021-11-14 18:18:28 +0300

    Prolog task 5 deleted unnecessary rule

--- tasks/prolog/5_merge.pl
+++ tasks/prolog/5_merge.pl
@@ -5,7 +5,6 @@ mrg([X|List1], [Y|List2], [X|Result]) :- X < Y, mrg(List1, [Y|List2], Result).
 mrg([X|List1], [Y|List2], [Y|Result]) :- X >= Y, mrg([X|List1], List2, Result).
 mrg([], List2, List2).
 mrg(List1, [], List2).
-mrg([], [], []).
 
 % ?- mrg([1, 2, 5, 6, 7], [3, 4, 5, 8], Result).
 % Result = [1, 2, 3, 4, 5, 5, 6, 7, 8] 

8abe8be984bf3428223cb3e155932e9d9115cd5f
natalya-revtova <revtovagn@gmail.com>
2021-11-20 16:18:38 +0300

    add scala lab 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,6 +1,3 @@
-package recfun
-import common._
-
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -9,20 +6,40 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println()
+    println("Balance")
+    println(balance("((()))".toList))          // true
+    println(balance("((())))".toList))         // false
+    println(balance("(3+4)*(1+2)".toList))     // true
+    println(balance("(3+4)*((1+2)".toList))    // false
+
+    println()
+    println("Count Change")
+    println(countChange(5, List()))         // 0
+    println(countChange(-1, List(1,2,3)))   // 0
+    println(countChange(4, List(1,2,3)))    // 4
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def indexPar(chars: List[Char], parBalance: Int): Int = {
+      if (chars.isEmpty || parBalance < 0) parBalance
+      else if (chars.head == ')') indexPar(chars.tail, parBalance - 1)
+      else if (chars.head == '(') indexPar(chars.tail, parBalance + 1)
+      else indexPar(chars.tail, parBalance)
+    }
+    indexPar(chars, 0) == 0
   }
 
   /**
@@ -33,6 +50,11 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    def count(money: Int, coins: List[Int]): Int = {
+      if (money == 0) 1
+      else if (money > 0 && coins.nonEmpty) count(money - coins.head, coins) + count(money, coins.tail)
+      else 0
+    }
+    count(money, coins)
   }
 }

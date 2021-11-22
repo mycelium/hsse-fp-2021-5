8a780477813354eafdf79407e3676aec15d4a2f2
Zhukova Ksenia <zhukova.ksenia2000@gmail.com>
2021-11-23 00:12:12 +0300

    Add Haskell task

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,10 @@
 module Hamming (distance) where
 
+import Data.Maybe
+
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys = 
+    if length xs /= length ys then Nothing
+    else if null xs || null ys then Just 0
+    else if head xs == head ys then distance (tail xs) (tail ys)
+    else Just (sum . catMaybes $ [Just 1, distance (tail xs) (tail ys)])

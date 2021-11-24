55faa0a7640955ffb04849f6c8af89316b9ca580
Sabutkevich Artem <artem.sabut@gmail.com>
2021-11-24 23:30:20 +0300

    Define predicates

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -1,12 +1,18 @@
 % определить предикаты:
 	% and(A,B)
+	and(A,B):- A, B.
 	% or(A, B)
-	% xor(A, B)
-	% not(A)
+	or(A,B):- A; B.
+	% xor(A,B)
+	xor(A,B):- or(and(A,not(B)),and(not(A),B)).
+	% not(A) - уже определен
 	% equ(A,B)
+	equ(A,B):- or(and(not(A),not(B)),and(A,B))
+	
 % ипользовать предикат truth_table(A,B, expression) для построения таблиц истинности, например:
 % truth_table(A,B,and(A,or(A,B))).
 % true true true
 % true fail true
 % fail true fail
 % fail fail fail
+

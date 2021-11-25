5d7c004f55471994d0d5df44a1587ab2e1853b08
Kristijan271197 <59137001+Kristijan271197@users.noreply.github.com>
2021-11-25 13:30:38 +0300

    Prolog Lab 2

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -1,12 +1,12 @@
-% определить предикаты:
-	% and(A,B)
-	% or(A, B)
-	% xor(A, B)
-	% not(A)
-	% equ(A,B)
-% ипользовать предикат truth_table(A,B, expression) для построения таблиц истинности, например:
-% truth_table(A,B,and(A,or(A,B))).
-% true true true
-% true fail true
-% fail true fail
-% fail fail fail
+cnot(true, fail).
+cnot(fail,true).
+cand(fail,fail,fail).
+cand(fail,true,fail).
+cand(true,fail,fail).
+cand(true,true,true).
+cor(fail,fail,fail).
+cor(fail,true,true).
+cor(true,fail,true).
+cor(true,true,true).
+cxor(X, Y, Z) :- cnot(X, N1), cnot(Y, N2),cand(X, N2, N3), cand(Y, N1, N4), cand(X, N2, N3), cand(Y, N1, N4), cor(N3, N4, Z).
+?-cxor(I1,I2,S),write(I1), write(" "), write(I2), write(" "), write(S), write(" "), nl, fail.

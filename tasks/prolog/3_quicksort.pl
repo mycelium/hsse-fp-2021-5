be8bad6f5922d26b5fac9b818ef057b5865ced9f
ponomarev.as <a.s.ponomarev.mail@gmail.com>
2021-11-25 17:07:31 +0300

    fix signature of func

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,11 +1,11 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
 
-quicksort([],[]).
+qsort([],[]).
 
-quicksort([X|Tail], Sorted) :-
+qsort([X|Tail], Sorted) :-
     split(X, Tail, Small, Big),
-	quicksort(Small, SortedSmall),
-	quicksort(Big, SortedBig),
+	qsort(Small, SortedSmall),
+	qsort(Big, SortedBig),
 	append(SortedSmall, [X|SortedBig], Sorted).
 
 split(_, [], [], []).
@@ -15,8 +15,8 @@ split(X, [Y|Tail], [Y|Small], Big) :-
 split(X, [Y|Tail], Small, [Y|Big]) :-
 	split(X, Tail, Small, Big).
 
-% ? - quicksort([10, 5, 0, 6, 8, -13], Sorted)
+% ? - qsort([10, 5, 0, 6, 8, -13], Sorted)
 % K = [-13, 0, 5, 6, 8, 10]
 
-% ? - quicksort([10, 5, 0, 6, 8, -13], [-13, 0, 5, 6, 8, 10])
+% ? - qsort([10, 5, 0, 6, 8, -13], [-13, 0, 5, 6, 8, 10])
 % K = true

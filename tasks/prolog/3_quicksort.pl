257d99b121e96e53a55bd6dee8972b99aba1e514
mila_shumkovskaya <mila_shumkovskaya@mail.ru>
2021-11-27 22:04:05 +0300

    lab 3 prolog

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,17 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+divide(_, [],[],[]).
+divide(X, [H|T], [H|Small], Big) :-
+    X > H,
+    divide(X, T, Small, Big).
+divide(X, [H|T], Small, [H|Big]) :-
+    divide(X, T, Small, Big).
+	
+qsort([],[]).   
+qsort([H|T], Sorted) :-         
+   divide(H, T, Small, Big),
+    qsort(Small, SmallS),
+    qsort(Big, BigS),
+    append(SmallS, [H|BigS], Sorted).
+
+

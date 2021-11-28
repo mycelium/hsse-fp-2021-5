ad620a14fa2535bb5adce8e9203a3df5c2ef0a89
mila_shumkovskaya <mila_shumkovskaya@mail.ru>
2021-11-28 18:14:38 +0300

    lab 2 scala

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -2,5 +2,15 @@ package funsets
 
 object Main extends App {
   import FunSets._
+
+  val s = Set(1,2,3,4)
+  val t = Set(7,6,5,4,3)
+
   println(contains(singletonSet(1), 1))
+  printSet(union(s,t))
+  printSet(intersect(s,t))
+  printSet(diff(s,t))
+  printSet(filter(s, elem => elem < 3))
+  println(exists(s, elem => elem < 3))
+  printSet(map(t, elem => elem * 2))
 }

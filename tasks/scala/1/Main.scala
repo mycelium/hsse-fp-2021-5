21cfb51b4df97262d0bbd63bc2368bf903da8037
mila_shumkovskaya <mila_shumkovskaya@mail.ru>
2021-11-28 11:18:37 +0300

    lab 1 scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,5 @@
 package recfun
-import common._
+//import common._
 
 object Main {
   def main(args: Array[String]) {
@@ -9,20 +9,45 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+    println(balance("(2+3)*4+(7*(9-4))".toList))
+    println(balance("(2+3)*4)(".toList))
+    println(balance("(2+3(*4)".toList))
+    println(countChange(5, List(2,3) ))
+    println(countChange(5, List(2,3,1) ))
+    println(countChange(37, List(1, 5, 10, 25) ))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    assert(c >= 0 && c <= r)
+    if (c == 0 || c == r)
+      return 1
+    return pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
-  def balance(chars: List[Char]): Boolean = {
-   
+   def balance(chars: List[Char]): Boolean = {
+        return checkBalance(chars, 0)
+   }
+
+  def checkBalance(chars: List[Char], counter: Int): Boolean = {
+    if(chars.isEmpty){
+      return counter==0
+    }
+    if(counter<0) {
+      return false
+    }
+    if(chars.head=='(') {
+      return checkBalance(chars.tail, counter+1)
+    }
+    if(chars.head==')') {
+      return checkBalance(chars.tail, counter-1)
+    }
+    return checkBalance(chars.tail, counter)
   }
 
   /**
@@ -33,6 +58,10 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    val sortedCoins = coins.sortWith(_ > _)
+    if (money < 0 || coins.isEmpty) return 0;
+    if (money == 0) return 1;
+    return countChange(money, sortedCoins.tail) + countChange(money - sortedCoins.head, sortedCoins)
   }
+
 }

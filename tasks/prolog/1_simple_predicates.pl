034152797a14a2ebbccf0ccd39a5ec835da868a2
CarloMagazoni <58231398+CarloMagazoni@users.noreply.github.com>
2021-11-30 16:38:27 +0300

    Add prolog_1

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -1,5 +1,3 @@
-% есть набор фактов вида father(person1, person2) (person1 is the father of person2)
-% Необходимо определить набор предикатов:
 % 1. brother(X,Y)    -  определяющий являются ли аргументы братьями
 % 2. cousin(X,Y)     -  определяющий являются ли аргументы двоюродными братьями
 % 3. grandson(X,Y)   -  определяющий является ли аргумент Х внуком аргумента Y
@@ -10,8 +8,65 @@
 	father(b,d).  % 3
 	father(b,e).  % 4
 	father(c,f).  % 5
-% указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+	
+	brother(X,Y):-
+		father(Z,X),
+		father(Z,Y),X\=Y.
+	cousin(X,Y):-
+		brother(M,Z),
+		father(Z,Y),
+		father(M,X).
+	grandson(X,Y):-
+		father(Y,Z),
+		father(Z,X).
+	descendent(X,Y):- father(X,Y).
+	descendent(X,Y):- 
+		father(Z,X),
+		descendent(Z,Y).
+
+?- brother(X,Y).
+X = b,
+Y = c ;
+X = c,
+Y = b ;
+X = d,
+Y = e ;
+X = e,
+Y = d ;
+false.
+?- cousin(X,Y).
+X = d,
+Y = f ;
+X = e,
+Y = f ;
+X = f,
+Y = d ;
+X = f,
+Y = e ;
+false.
+?- grandson(X,Y).
+X = d,
+Y = a ;
+X = e,
+Y = a ;
+X = f,
+Y = a ;
+false.
+?- descendent(X,Y).
+X = b,
+Y = a ;
+X = c,
+Y = a ;
+X = d,
+Y = b ;
+X = e,
+Y = b ;
+X = f,
+Y = c ;
+X = d,
+Y = a ;
+X = e,
+Y = a ;
+X = f,
+Y = a ;
+false.

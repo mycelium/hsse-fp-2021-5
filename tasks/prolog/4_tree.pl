07b78a0a6bc56c0b37e4564bce3f9e4c3a1c0cee
Sabutkevich Artem <artem.sabut@gmail.com>
2021-12-05 01:22:06 +0300

    Add balanced_tree

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,20 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+:- ['3_quicksort.pl'].
+
+len([],0).
+len([H|T],LEN):- len(T,N), LEN is N+1.
+
+% Подбираем такой LT, чтобы его длина была равна половине исходного K.
+% RT подбираем так, чтобы если к выбранному LT приставить RT получился исходный K.  
+
+divide_tree(K,LT,RT):- len(K,LEN), X is LEN//2, len(LT,X), append(LT,RT,K).
+
+tree([],empty).
+tree(K,instant(ROOT,L,R)):- divide_tree(K,LT,[ROOT|RT]), tree(LT,L), tree(RT,R).
+
+% Пустое бинарное дерево всегда сбалансированное.
+balanced_tree([],T).
+balanced_tree(L,T):- qsort(L,K), tree(K,T).

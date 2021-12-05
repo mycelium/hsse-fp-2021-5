bdeded6fe94d3f69bcf5da4b561565bb42070f78
Marina <Alekseeva.Marina1234@yandex.ru>
2021-12-05 22:46:16 +0300

    lab 5 done

--- tasks/prolog/5_merge.pl
+++ tasks/prolog/5_merge.pl
@@ -1,2 +1,8 @@
 $ определить предикат mrg(L1, L2, R) который для двух отсортированных списков L1 и L2 
 % определяет список R, составленный из этих элементов
+
+
+mrg(A, [], A).
+mrg([], B, B).
+mrg([A|TailA], [B|TailB], [A|TailMerged]) :- A =< B, mrg(TailA, [B|TailB], TailMerged).	
+mrg([A|TailA], [B|TailB], [B|TailMerged]) :- A > B, mrg([A|TailA], TailB, TailMerged).

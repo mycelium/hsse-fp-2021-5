4676832e742bb85e19bb3c42d38a4f891fdb9c06
Marina <Alekseeva.Marina1234@yandex.ru>
2021-12-05 22:37:32 +0300

    lab 3 done

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,15 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+
+qsort([],[]).   
+
+% Left - элементы из Tail меньше X, Right - больше => рекурсия => 
+% результирующий список: отсортированная левая часть, X, отсортированная правая
+
+qsort([X|Tail], SortedResult) :-         
+	split_(X, Tail, Left, Right), qsort(Left, SortedLeft),
+	qsort(Right, SortedRight), append(SortedLeft, [X|SortedRight], SortedResult).
+	
+split_(X, [],[],[]).
+split_(X, [Y|Tail], [Y|Left], Right) :- X > Y, split_(X, Tail, Left, Right).
+split_(X, [Y|Tail], Left, [Y|Right]) :- split_(X, Tail, Left, Right).

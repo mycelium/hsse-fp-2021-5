56a3f34097beeb01b7b3fdf39f6ca0cd30bd0ffb
yfrsaul <alekseyev.vlad@yandex.ru>
2021-12-07 01:42:51 +0300

    Added the second prolog lab

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -1,12 +1,21 @@
 % определить предикаты:
-	% and(A,B)
-	% or(A, B)
-	% xor(A, B)
-	% not(A)
-	% equ(A,B)
+	and(A, B) :- A, B.
+	or(A, B) :- A ; B.
+	xor(A, B) :- (not(A),B) ; (A, not(B)).
+	% not(A) is the same as not(A).
+	equ(A, B) :- A = B.
 % ипользовать предикат truth_table(A,B, expression) для построения таблиц истинности, например:
 % truth_table(A,B,and(A,or(A,B))).
 % true true true
 % true fail true
 % fail true fail
 % fail fail fail
+
+bool(true).
+bool(false).
+
+truth_table(A, B, Expression) :-
+    bool(A), bool(B),
+	(write(A), write('\t'), write(B), write('\t')),
+	(Expression -> write('true'); write('false')),
+	nl, fail.

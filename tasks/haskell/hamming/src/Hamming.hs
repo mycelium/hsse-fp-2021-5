84941ade8c7d4e12418ac3098894cb4e742e9e4b
CarloMagazoni <58231398+CarloMagazoni@users.noreply.github.com>
2021-12-06 13:17:58 +0300

    First haskell lab | No custom tests

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,11 @@
 module Hamming (distance) where
+import Control.Monad (liftM2)
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+--distance xs ys = error "Implementation is lost..."
+--Implementation:
+distance xs ys
+	| length xs /= length ys = Nothing
+    | xs == [] = Just 0
+    | head xs /= head ys = liftM2 (+) (Just 1) (distance (tail xs) (tail ys))
+    | otherwise = distance (tail xs) (tail ys)

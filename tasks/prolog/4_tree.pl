1d190e51fe882461c03aa28797ac20a8a43f3d7d
yfrsaul <alekseyev.vlad@yandex.ru>
2021-12-07 22:47:39 +0300

    Added the fourth prolog lab

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,37 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+qsort(L,K):-
+		L = [Head|Tail],
+		partition(Head, Tail, List1, List2),
+		qsort(List1, Sorted1),
+		qsort(List2, Sorted2),
+		append(Sorted1, [Head|Sorted2], K).
+
+qsort([], []).
+
+partition(Current, [Head|Tail], [Head|LowEq], High):-
+		Current >= Head,
+		partition(Current, Tail, LowEq, High).
+
+partition(Current, [Head|Tail], LowEq, [Head|High]):-
+		partition(Current, Tail, LowEq, High).
+
+partition(_, [], [], []).
+
+split(List, Left, Right):-
+	append(Left, Right, List),
+	length(List, Len),
+	Half is Len  div 2,
+	length(Left, Half).
+
+balanced_tree_impl([], empty).
+balanced_tree_impl(List, instant(Root, LeftLeef, RightLeef)):-
+	split(List, LeftList, [Root|RightList]),
+	balanced_tree_impl(LeftList, LeftLeef),
+	balanced_tree_impl(RightList, RightLeef).
+
+balanced_tree(L, T):-
+	qsort(L, SL),
+	balanced_tree_impl(SL, T).

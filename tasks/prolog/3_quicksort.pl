103247901181a1bcc00be502474efc0c9a7c765b
yfrsaul <alekseyev.vlad@yandex.ru>
2021-12-07 16:37:40 +0300

    Added the third prolog lab

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,18 @@
-% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+% Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный
+qsort(L,K):-
+		L = [Head|Tail],
+		partition(Head, Tail, List1, List2),
+		qsort(List1, Sorted1),
+		qsort(List2, Sorted2),
+		append(Sorted1, [Head|Sorted2], K).
+
+qsort([], []).
+
+partition(Current, [Head|Tail], [Head|LowEq], High):-
+		Current >= Head,
+		partition(Current, Tail, LowEq, High).
+
+partition(Current, [Head|Tail], LowEq, [Head|High]):-
+		partition(Current, Tail, LowEq, High).
+
+partition(_, [], [], []).

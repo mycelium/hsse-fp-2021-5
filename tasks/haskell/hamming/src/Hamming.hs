51d439a54edc49470e76e0bf9d3e74fc3e22aab1
Sabutkevich Artem <artem.sabut@gmail.com>
2021-12-09 13:15:53 +0300

    Fix hamming relatively origin tests

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,7 +1,10 @@
 module Hamming (distance) where
 
+import Control.Monad
+
 distance :: String -> String -> Maybe Int
 distance xs ys = 
     if xs == [] && ys == [] then Just 0
-    if head xs == head ys then distance (tail xs) (tail ys)
-
+    else if length xs /= length ys then Nothing
+    else if head xs == head ys then distance (tail xs) (tail ys)
+    else fmap(+1) (distance (tail xs) (tail ys))

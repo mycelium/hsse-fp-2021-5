0692c54e27cc3c24f471e61724879a5755a39c5f
Pavel <pahsagess2@mail.ru>
2021-12-09 23:53:25 +0300

    Added scala labs

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,5 @@
 package recfun
-import common._
+import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
@@ -9,20 +9,38 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+    println("Balance")
+    println(balance("(())".toList))
+    println(balance("(()".toList))
+    println(balance("(*_*)".toList))
+    println(balance(":-(".toList))
+
+    println("Count Change")
+    println(countChange(5, List(2,3)))
+    println(countChange(0, List(1,2,3)))
+    println(countChange(6, List(1,2,3)))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    @tailrec
+    def loop(chars: List[Char], parentheses: Int): Boolean = chars match {
+      case Nil => parentheses == 0
+      case ')' :: tail => if(parentheses==0) false else loop(tail, parentheses-1)
+      case '(' :: tail  => loop(tail, parentheses+1)
+      case head :: tail  => loop(tail, parentheses)
+    }
+    loop(chars,0)
   }
 
   /**
@@ -33,6 +51,13 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+      def loop(money: Int, coins: List[Int], ways:Int): Int = coins match {
+      case Nil => ways
+      case _ if money == 0 => ways+1
+      case _ if(money - coins.head < 0 || coins.isEmpty) => 0
+      case _ => loop(money-coins.head, coins, ways) +   loop(money, coins.tail, ways)
+    }
+    val sortedCoins = coins.sorted
+    loop(money, sortedCoins, 0)
   }
 }
--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -2,5 +2,14 @@ package funsets
 
 object Main extends App {
   import FunSets._
+  val s = Set(1, 2, 3, 42, 78)
+  val t = Set(1, 2, 6, 9, 14)
+
   println(contains(singletonSet(1), 1))
+  printSet(union(s,t))
+  printSet(intersect(s,t))
+  printSet(diff(s,t))
+  printSet(filter(s, t))
+  println(exists(t, elem => elem > 5))
+  printSet(map(s, elem => elem + 3))
 }

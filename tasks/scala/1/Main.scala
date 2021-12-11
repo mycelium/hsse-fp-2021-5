e584e6b65c49aaf51895dc5ca5eaf754f3d3a5b5
Sabutkevich Artem <artem.sabut@gmail.com>
2021-12-11 14:07:06 +0300

    Add pascals triangle from exercise 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,8 +1,5 @@
-package recfun
-import common._
-
 object Main {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
@@ -13,26 +10,25 @@ object Main {
 
   /**
    * Exercise 1
+   Реализация с использованием биномиальных коэффициентов не прошла по памяти.
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
-
+  
   /**
-   * Exercise 2 Parentheses Balancing
+     Pascal's Triangle
+     1
+     1 1
+     1 2 1
+     1 3 3 1
+     1 4 6 4 1
+     1 5 10 10 5 1
+     1 6 15 20 15 6 1
+     1 7 21 35 35 21 7 1
+     1 8 28 56 70 56 28 8 1
+     1 9 36 84 126 126 84 36 9 1
+     1 10 45 120 210 252 210 120 45 10 1
    */
-  def balance(chars: List[Char]): Boolean = {
-   
-  }
-
-  /**
-   * Exercise 3 Counting Change
-   * Write a recursive function that counts how many different ways you can make
-   * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
-   * 2 and 3: 2+3.
-   */
-  def countChange(money: Int, coins: List[Int]): Int = {
-
-  }
 }

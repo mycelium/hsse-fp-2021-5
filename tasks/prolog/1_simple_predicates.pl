8275904aef1865b95875b42af2641eb72798c8a7
Alina923 <lastochkina.aa@edu.spbstu.ru>
2021-12-11 23:31:38 +0300

    prolog. 1 lab is ready

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -15,3 +15,47 @@
 	?- cousin(X,Y).
 	?- grandson(X,Y).
 	?- descendent(X,Y).
+ather(a,b). % 1
+father(a,c). % 2
+father(b,d). % 3
+father(b,e). % 4
+father(c,f). % 5
+
+
+brother(X,Y) :- father(Z,X), father(Z,Y), X \= Y.
+
+cousin(X,Y) :- father(W,X), father(Z,Y), brother(W,Z).
+
+grandson(X,Y) :- father(Y,Z), father(Z,X).
+
+descendent(X,Y) :- father(Y,X).
+
+descendent(X,Y) :- father(Y,Z), descendent(X,Z).
+/* brother(X,Y).
+X Y
+b c 1
+c b 2
+d e 3
+e d 4
+cousin(X,Y).
+X Y
+d f 1
+e f 2
+f d 3
+f e 4
+grandson(X,Y).
+X Y
+d a 1
+e a 2
+f a 3
+descendent(X,Y)
+X Y
+b a 1
+c a 2
+d b 3
+e b 4
+f c 5
+d a 6
+e a 7
+f a 8
+*/

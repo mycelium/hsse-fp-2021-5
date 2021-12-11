5b8c300c1fc104f6e96a1fff6ff5db11ccc7884d
Alina923 <lastochkina.aa@edu.spbstu.ru>
2021-12-12 00:29:12 +0300

    haskell. lab is ready

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,13 @@
 module Hamming (distance) where
 
+import Data.Char
+
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys =
+    --  строки одинаковой длины, содержат только символы и символы в строке в верхнем регистре.
+    if length xs == length ys  && all isLetter (xs ++ ys) && all isUpper  (xs ++ ys)
+    then
+		  --  сравниваем символы на неравенство и фильтруем значения только для True, считаем их кол-во
+        Just ( length (filter (==True) (zipWith (/=) xs ys)) )
+    else
+        Nothing

b2ee36faf34547fd56e4e09dc56cf043ebd13708
Alina923 <lastochkina.aa@edu.spbstu.ru>
2021-12-12 00:26:43 +0300

    skala. 2 lab is ready

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -1,6 +1,26 @@
 package funsets
 
+import funsets.FunSets.toString
+
 object Main extends App {
+
   import FunSets._
-  println(contains(singletonSet(1), 1))
+
+  val setA = union(singletonSet(1), union(singletonSet(2), singletonSet(3)))
+  val setB = union(singletonSet(3), union(singletonSet(4), singletonSet(5)))
+  val unionAB = union(setA, setB)
+  val intersectionAB = intersect(setA, setB)
+  val differenceAB = diff(setA, setB)
+  val filteredOdd = filter(setA, x => x % 2 != 0)
+  val setAContainsEven = exists(setA, x => x % 2 == 0)
+  val setASquared = map(setA, x => x * x)
+
+  println(s"A: ${FunSets.toString(setA)}")
+  println(s"B: ${FunSets.toString(setB)}")
+  println(s"union: ${FunSets.toString(unionAB)}")
+  println(s"intersection: ${FunSets.toString(intersectionAB)}")
+  println(s"difference: ${FunSets.toString(differenceAB)}")
+  println(s"filtered odd: ${FunSets.toString(filteredOdd)}")
+  println(s"A contains even: ${setAContainsEven}")
+  println(s"map squared: ${FunSets.toString(setASquared)}")
 }

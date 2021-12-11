d6b2247dd1379bbe4a3e6bf1ed2a8f0a2745b285
Alina923 <lastochkina.aa@edu.spbstu.ru>
2021-12-11 23:35:51 +0300

    skala. 1 lab is ready

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -9,20 +9,43 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    val balanced = "((2+3)*5+(5+2)/4)".toList
+    val notBalanced = "(((2+5)".toList
+    println(s"balance good: ${balance(balanced)}\nbalance bad: ${balance(notBalanced)}")
+
+    val coins = List(3, 5)
+    println(s"change: ${countChange(10, coins)}")
+    println(s"change: ${countChange(15, coins)}")
   }
 
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
+  def pascal(column: Int, row: Int): Int = {
+    if (column < 0 || row < 0) {
+      throw new IllegalArgumentException("Column and row must be positive.")
+    } else if (column > row) {
+      throw new IllegalArgumentException("Row number must be greater than column.")
+    }
 
+    if (column == 0 || column == row) 1
+    else pascal(column - 1, row - 1) + pascal(column, row - 1)
   }
+
 
   /**
    * Exercise 2 Parentheses Balancing
    */
-  def balance(chars: List[Char]): Boolean = {
-   
+  def balance(expressionChars: List[Char]): Boolean = {
+    def indexParentheses(chars: List[Char], parenthesesBalance: Int): Int = {
+      if (chars.isEmpty || parenthesesBalance < 0) parenthesesBalance
+      else if (chars.head == ')') indexParentheses(chars.tail, parenthesesBalance - 1)
+      else if (chars.head == '(') indexParentheses(chars.tail, parenthesesBalance + 1)
+      else indexParentheses(chars.tail, parenthesesBalance)
+    }
+
+    indexParentheses(expressionChars, 0) == 0
   }
 
   /**
@@ -33,6 +56,12 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    def change(money: Int, coins: List[Int]): Int = {
+      if (money < 0 || coins.isEmpty) 0
+      else if (money == 0) 1
+      else change(money, coins.tail) + change(money - coins.head, coins)
+    }
 
+    change(money, coins)
   }
 }

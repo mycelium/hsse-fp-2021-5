88f63b473ac32cfe50992b8e5044553bf071f683
Sherzod Mamadaliev <mamadaliev@yahoo.com>
2021-12-13 15:04:55 +0300

    Fix the prolog laboratory work 4

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -4,28 +4,31 @@
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
 
-:- consult('3_quicksort.pl').
+pivot(_, [], [], []).
+pivot(Pivot, [L|Tail], [L|LTE], GT) :- Pivot >= L, pivot(Pivot, Tail, LTE, GT). 
+pivot(Pivot, [L|Tail], LTE, [L|GT]) :- pivot(Pivot, Tail, LTE, GT).
 
-% balanced_tree
-balanced_tree(L, T) :- qsort(L, K), build_tree(K, T).
+qsort([], []).
+qsort([L|Tail], K) :- pivot(L, Tail, L1, L2), qsort(L1, K1), qsort(L2, K2), append(K1, [L|K2], K).
 
-% build_tree
-build_tree([], empty).
-build_tree(L, instant(X, T1, T2)) :- split(L, A, [X|B]), build_tree(A, T1), build_tree(B, T2).
+balanced_tree(L, T)  :- qsort(L, K), tree_build(K, T).
 
-% split
-split(L, A, B) :- length(L, N), H is N - N // 2, length(B, H), append(A, B, L).
+tree_build(L, instant(H, T1, T2)) :- splite(L, L1, [H|Tail]), tree_build(L1, T1), tree_build(Tail, T2).
+tree_build([], empty).
+
+splite(L, L1, L2) :- splite2(L, L, L1, L2).
+splite2([H1|L1], [_,_|L2], [H1|T1], R) :- splite2(L1, L2, T1, R).
+splite2([H1|L1], [_], T1, [H1|R]) :- splite2(L1, [], T1, R).
+splite2(L1, [], [], L1).
 
 /*
-swipl .\4_tree.pl
-
 ?- balanced_tree([1, 3, 6, 8, 10, 14], T).
 T = instant(8, instant(3, instant(1, empty, empty), instant(6, empty, empty)), instant(14, instant(10, empty, empty), empty)) 
 
         8
       /   \
     3      14
-  /   \       \
- 1     6       10
+  /   \    /
+ 1     6  10   
 
 */

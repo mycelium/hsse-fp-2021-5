3b0ce0dfc5ca8a5c512dc70220b425b36f1e71c0
RazuvaevDD <Razuvaev_DD@mail.ru>
2021-12-14 18:10:54 +0300

    1 simple predicates: added descendent

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -12,13 +12,17 @@
 	father(c,f).  % 5
 
 % 1.
-	brother(X,Y):- father(Z,X), father(Z,Y), not(X = Y).
+	brother(X,Y):- father(Z,X), father(Z,Y), not(X=Y).
 
 % 2.
 	cousin(X,Y):- father(Z,X), father(J,Y), brother(Z,J).
 
 % 3.
 	grandson(X,Y):- father(Y,Z), father(Z,X). 
+
+% 4.
+	descendent(X,Y):- father(Y,X).
+	descendent(X,Y):- father(Z,X), descendent(Z,Y).
 
 % указать в каком порядке и какие ответы генерируются вашими методами
 
@@ -48,6 +52,26 @@
 
 % 3.
 	% ?- grandson(X,Y).
+	% X = d,
+	% Y = a ;
+	% X = e,
+	% Y = a ;
+	% X = f,
+	% Y = a ;
+	% false.
+
+% 4.
+	% ?- descendent(X,Y).
+	% X = b,
+	% Y = a ;
+	% X = c,
+	% Y = a ;
+	% X = d,
+	% Y = b ;
+	% X = e,
+	% Y = b ;
+	% X = f,
+	% Y = c ;
 	% X = d,
 	% Y = a ;
 	% X = e,

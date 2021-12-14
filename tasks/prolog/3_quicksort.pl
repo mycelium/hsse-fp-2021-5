983ead9aea4068eee59a1ac401248ba8dc6785cc
tkachuk andrey <tka4uk.andrei@yandex.ru>
2021-12-14 04:41:09 +0300

    task 3 is done

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,15 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+% Замечание. Для простоты выбираем первый элемент (см. сортировка Хоара)
+
+qsort([], []).
+qsort([H|T], K) :- 
+    partition(H, T, Less, More),
+    qsort(Less, SortedLess),
+    qsort(More, SortedMore),
+    append([SortedLess, [H], SortedMore], K).
+
+% из одного списка формируем два путём проверки на неравенство элементов списка опорным
+partition(Pivot, [H|T], [H|Less], More) :- H =< Pivot, partition(Pivot, T, Less, More).
+partition(Pivot, [H|T], Less, [H|More]) :- H > Pivot,  partition(Pivot, T, Less, More).
+partition(_, [], [], []).

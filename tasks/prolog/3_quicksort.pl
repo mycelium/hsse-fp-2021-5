556ff77641d84676bebdd3398400fa8b10d458a0
Vladimir <34841040+VladimirOrlov9@users.noreply.github.com>
2021-12-19 21:01:43 +0300

    Fixed second part of lab3

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -5,8 +5,13 @@ qsort([X], [X]).
 qsort([P|T], RES):-
   divide(T, P, X, Y),
   qsort(X, SortedX),
-  qsort(Y, SortedY),!,
+  qsort(Y, SortedY), !,
   append(SortedY, [P|SortedX], RES).
 
-% ?- qsort([1, 3, 2, -2, 23,4], Res).
+divide([], _, [], []) :- !.
+divide([H|T], P, [H|X], Y) :- H >= P, !, divide(T, P, X, Y).
+divide([H|T], P, X, [H|Y]) :- divide(T, P, X, Y).
+
+
+% ?- qsort([1, 3, 2, -2, 23, 4], Res).
 % Res = [-2, 1, 2, 3, 4, 23].

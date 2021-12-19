518a331c3a056f4dcda4a3590c67bb3bd672eb5a
Vladimir <34841040+VladimirOrlov9@users.noreply.github.com>
2021-12-19 20:11:37 +0300

    Add lab3 solution

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,12 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+qsort([], []).
+qsort([X], [X]).
+qsort([P|T], RES):-
+  divide(T, P, X, Y),
+  qsort(X, SortedX),
+  qsort(Y, SortedY),!,
+  append(SortedY, [P|SortedX], RES).
+
+% ?- qsort([1, 3, 2, -2, 23,4], Res).
+% Res = [-2, 1, 2, 3, 4, 23].

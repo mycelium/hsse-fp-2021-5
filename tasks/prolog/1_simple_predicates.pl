366862fac6e7ae38f4ec225d954414ebb4e25d9c
liurui456 <90747353+liurui797400liurui@users.noreply.github.com>
2021-12-20 15:50:03 +0300

    Add files via upload

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -10,8 +10,63 @@
 	father(b,d).  % 3
 	father(b,e).  % 4
 	father(c,f).  % 5
+	
+	
+	brother(X,Y):-father(Z,X),father(Z,Y),X\=Y.
+	cousin(X,Y):-brother(C,Z),father(Z,Y),father(C,X).
+	grandson(X,Y):-father(Y,Z),father(Z,X).
+	descendent(X,Y):- father(X,Y).
+	descendent(X,Y):-father(Z,X),descendent(Z,Y).
+	
+	
+	
+	
+	
 % указать в каком порядке и какие ответы генерируются вашими методами
-	?- brother(X,Y).
-	?- cousin(X,Y).
-	?- grandson(X,Y).
-	?- descendent(X,Y).
+?- brother(X,Y).
+X = b,
+Y = c ;
+X = c,
+Y = b ;
+X = d,
+Y = e ;
+X = e,
+Y = d ;
+false.
+?- cousin(X,Y).
+X = d,
+Y = f ;
+X = e,
+Y = f ;
+X = f,
+Y = d ;
+X = f,
+Y = e ;
+false.
+?- grandson(X,Y).
+X = d,
+Y = a ;
+X = e,
+Y = a ;
+X = f,
+Y = a ;
+false.
+?- descendent(X,Y).
+X = b,
+Y = a ;
+X = c,
+Y = a ;
+X = d,
+Y = b ;
+X = e,
+Y = b ;
+X = f,
+Y = c ;
+X = d,
+Y = a ;
+X = e,
+Y = a ;
+X = f,
+Y = a ;
+false.
+	

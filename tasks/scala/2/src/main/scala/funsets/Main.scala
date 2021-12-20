1f7ea3a6c65290b71952194b5f012035b8ff7154
Repin Boris <brepin@inbox.ru>
2021-12-21 00:09:29 +0300

    add scala lab 2

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,11 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  printSet(union(singletonSet(10), x => x < 5 && x > -5))
+  printSet(diff(x => x > 5 && x < 15, x => x > 10))
+  printSet(intersect(x => x > 0,  x => x < 5))
+  printSet(filter(x => x < 5 && x > -5, x => x > 0))
+  println(forall(x => x < 5 && x > -5, x => x < 5 && x > -5))
+  println(exists(x => x < 5 && x > -5, x => x > 5))
+  printSet(map(x => x < 5 && x > -5, x => 2*x))
 }

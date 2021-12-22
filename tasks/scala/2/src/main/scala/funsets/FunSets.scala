3d1ec256e6db4cc8fbb742d95a331b9f661c0157
ivanovvo <58865007+ivanovvo@users.noreply.github.com>
2021-12-22 21:33:39 +0300

    Update FunSets.scala

--- tasks/scala/2/src/main/scala/funsets/FunSets.scala
+++ tasks/scala/2/src/main/scala/funsets/FunSets.scala
@@ -20,67 +20,76 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
-  
+  def singletonSet(elem: Int): Set = setElem => elem == setElem
+
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
-  
+  def union(s: Set, t: Set): Set = setElem => s(setElem) || t(setElem)
+
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
-  
+  def intersect(s: Set, t: Set): Set = setElem => s(setElem) && t(setElem)
+
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
-  
+  def diff(s: Set, t: Set): Set = setElem => s(setElem) && !t(setElem)
+
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
-  
+  def filter(s: Set, p: Int => Boolean): Set = setElem => s(setElem) && p(setElem)
+
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
    */
   val bound = 1000
 
+
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
-    def forall(s: Set, p: Int => Boolean): Boolean = {
+  def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (s(a) && !p(a)) false
+      else if (a > bound) true
+      else iter(a+1)
     }
-    iter(???)
+
+    iter(-bound)
   }
-  
+
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
-  
+  def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, a => !p(a))
+
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
-  
+  def map(s: Set, f: Int => Int): Set = {
+    var resSet: Set = null;
+    forall(s, a => {
+      resSet =  if (resSet != null) union(singletonSet(f(a)), resSet)  else singletonSet(f(a))
+      true
+    })
+    resSet
+  }
+
   /**
    * Displays the contents of a set
    */
   def toString(s: Set): String = {
-    val xs = for (i <- -bound to bound if contains(s, i)) yield i
-    xs.mkString("{", ",", "}")
+    val n = for (i <- -bound to bound if contains(s, i)) yield i
+    n.mkString("{", ",", "}")
   }
 
   /**

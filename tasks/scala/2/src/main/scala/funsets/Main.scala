a56f340f346889b131c57986b1d83ed6d18300fa
Dramcryx <jopa19991@gmail.com>
2021-12-22 23:26:45 +0300

    3540904/10202 Vlasov Maksim Scala-2

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,9 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  printSet(map(union(x => x == 5, y=> y == 6), z => z + 3))
+  /*
+  [info] true
+  [info] {8,9}
+  */
 }

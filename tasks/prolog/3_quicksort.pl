973407304264705b3482f37a74938844fb4a3904
ivanovvo <58865007+ivanovvo@users.noreply.github.com>
2021-12-24 17:27:33 +0300

    Update 3_quicksort.pl

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,14 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+qsort([], []).
+qsort([H|T], S) :-
+    split(H, T, L, G),
+    qsort(L, SL),
+    qsort(G, SG),
+    append(SL, [H|SG], S).
+
+split(_, [], [], []).
+split(P, [H|T], L, [H|G]) :-
+    H > P, split(P, T, L, G).
+split(P, [H|T], [H|L], G) :-
+    H =< P, split(P, T, L, G).

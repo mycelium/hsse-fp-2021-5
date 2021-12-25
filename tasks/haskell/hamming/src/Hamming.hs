9f4e9d93c23d8e69746881f0db1d7ff14a3868da
Anastasia <anastasiareiz@gmail.com>
2021-12-25 07:10:34 -0800

    done Haskell

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,10 @@
 module Hamming (distance) where
 
+import Control.Monad
+
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys =
+    if null xs &&  null ys then Just 0
+    else if length xs /= length ys then Nothing
+    else if head xs == head ys then distance (tail xs) (tail ys)
+    else fmap(+1) (distance (tail xs) (tail ys))

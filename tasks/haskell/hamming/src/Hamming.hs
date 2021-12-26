4f04d33bfcd0e7d01c56304cecc76688306023b6
Dramcryx <jopa19991@gmail.com>
2021-12-27 01:06:45 +0300

    3540904/10202 Vlasov Maksim Haskell-1

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,11 @@
 module Hamming (distance) where
 
+import Data.List
+
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys
+    | length xs /= length ys = Nothing
+    | otherwise = Just (length (filter id (zipWith (/=) xs ys)))
+-- zipWith (/=) xs ys -> convert two strings to list of bools where non-equal are true
+-- filter id -> get only True values
+-- length -> take length of True's list

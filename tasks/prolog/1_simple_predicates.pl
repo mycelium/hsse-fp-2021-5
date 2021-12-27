aff1e45b99785198d2de3d3fa8da8a4b4ae08441
Ilya Burov <ilya.burov.99@mail.ru>
2021-12-27 11:36:49 +0300

    small fix lab1

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -1,11 +1,11 @@
 % есть набор фактов вида father(person1, person2) (person1 is the father of person2)
 % Необходимо определить набор предикатов:
 % 1. brother(X,Y)    -  определяющий являются ли аргументы брать¤ми
-% 2. cousin(X,Y)     -  определяющий являются ли аргументы двоюродными брать¤ми
+% 2. cousin(X,Y)     -  определяющий являются ли аргументы двоюродными братьями
 % 3. grandson(X,Y)   -  определяющий является ли аргумент X внуком аргумента Y
 % 4. descendent(X,Y) -  определяющий является ли аргумент X потомком аргумента Y
-% 5. использу¤ в качестве исходных данных следующий граф отношений
-    father(a,b).  % 1
+% 5. используя в качестве исходных данных следующий граф отношений
+       father(a,b).  % 1
 	father(a,c).  % 2
 	father(b,d).  % 3
 	father(b,e).  % 4
@@ -13,7 +13,7 @@
 
      % 1.
 
-     brother(X,Y):- father(Z,X), father(Z,Y), X\=Y.
+    brother(X,Y):- father(Z,X), father(Z,Y), X\=Y.
      % 2.
 
 	cousin(X,Y):- father(Z,X), father(P,Y), brother(Z,P).
@@ -22,10 +22,10 @@
 	grandson(X,Y):- father(Y,Z), father(Z,X).
      % 4.
 
-	descendent(X,Y):- father(Y,X).
-	descendent(X,Y):- father(F,X), descendent(F,Y).
+    descendent(X,Y):- father(Y,X).
+    descendent(X,Y):- father(F,X), descendent(F,Y).
 
-% указать в каком пор¤дке и какие ответы генерируютс¤ вашими методами
+% указать в каком порядке и какие ответы генерируютс¤ вашими методами
 	% ?- brother(X,Y).
 		% X = b,
 		% Y = c ;

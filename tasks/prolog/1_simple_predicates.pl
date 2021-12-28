1de2c1291af1b083c238377d07a50365854cae43
Maxim Klimenko <52748143+Infron@users.noreply.github.com>
2021-12-28 04:55:49 +0300

    Task 1 Prolog completed
    Upload Prolog Task 1

--- tasks/prolog/1_simple_predicates.pl
+++ tasks/prolog/1_simple_predicates.pl
@@ -11,52 +11,66 @@
 	father(b,e).  % 4
 	father(c,f).  % 5
 
-brother(X,Y):- father(Z,X),father(Z,Y), X\=Y.
-cousin(X,Y):- father(Q,X),father(Z,Q),father(E,Y),father(Z,E),Q\=E.
-grandson(X,Y):- father(Z,X),father(Y,Z).
-descendent(X,Y):- father(Z,X),descendent(Z,Y).
-descendent(X,Y):- father(Y,X).
-
 % указать в каком порядке и какие ответы генерируются вашими методами
-?- brother(X, Y).
-% X = b,
-% Y = c ;
-% X = c,
-% Y = b ;
-% X = d,
-% Y = e ;
-% X = e,
-% Y = d ;
+
+brother(X, Y):- father(Z, X), father(Z, Y), X\=Y.
+/*
+?- brother(X,Y).
+X = b,
+Y = c
+X = c,
+Y = b
+X = d,
+Y = e
+X = e,
+Y = d
+false
+*/
+
+cousin(X, Y):- father(Z, X), father(W, Y), brother(Z, W).
+/*
 ?- cousin(X,Y).
-% X = d,
-% Y = f ;
-% X = e,
-% Y = f ;
-% X = f,
-% Y = d ;
-% X = f,
-% Y = e ;
+X = d,
+Y = f
+X = e,
+Y = f
+X = f,
+Y = d
+X = f,
+Y = e
+false
+*/
+
+grandson(X,Y):- father(Z, X), father(Y, Z).
+/*
 ?- grandson(X,Y).
-% X = d,
-% Y = a ;
-% X = e,
-% Y = a ;
-% X = f,
-% Y = a.
+X = d,
+Y = a
+X = e,
+Y = a
+X = f,
+Y = a
+*/
+
+descendent(X,Y):- father(Y, X).
+descendent(X,Y):- father(Y, Z), father(Z, X).
+/*
 ?- descendent(X,Y).
-% X = d,
-% Y = a ;
-% X = e,
-% Y = a ;
-% X = f,
-% Y = a ;
-% X = b,
-% Y = a ;
-% X = c,
-% Y = a ;
-% X = d,
-% Y = b ;
-% X = e,
-% Y = b ;
-% X = f,
-% Y = c.
+X = b,
+Y = a
+X = c,
+Y = a
+X = d,
+Y = b
+X = e,
+Y = b
+X = f,
+Y = c
+X = d,
+Y = a
+X = e,
+Y = a
+X = f,
+Y = a
+false
+*/

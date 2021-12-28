a1092004c2560b388464f874b62c7905ac4bd172
Maxim Klimenko <52748143+Infron@users.noreply.github.com>
2021-12-28 05:01:05 +0300

    Task 4 Prolog completed
    Upload Prolog Task 4

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,30 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+qsort([], []).
+qsort([Head|Tail], K) :-
+    partition(Head, Tail, Less, More),
+    qsort(Less, SortedLess),
+    qsort(More, SortedMore),
+    append([SortedLess, [Head], SortedMore], K).
+    
+% Формируем два списка путем сравнивания с опорным элементом
+partition(Pivot, [Head|Tail], [Head|Less], More) :- Head =< Pivot, partition(Pivot, Tail, Less, More).
+partition(Pivot, [Head|Tail], Less, [Head|More]) :- Head > Pivot,  partition(Pivot, Tail, Less, More).
+partition(_, [], [], []).
+
+split(List, L, R):-
+    append(L, R, List),
+    length(List, ListLength),
+    LengthHalf is div(ListLength, 2),
+    length(L, LengthHalf).
+
+balanced_tree([], empty).
+
+balanced_tree(List, instant(Root, L, R)) :-
+    split(List, LeftPart, [Root|RightPart]),
+    balanced_tree(LeftPart, L),
+    balanced_tree(RightPart, R).
+
+balanced_tree(L,T):- qsort(L, LSorted), balanced_tree(LSorted, T).

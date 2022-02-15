5abbb9dfa6731d9ff7ec7fc294053f7694663f22
Maxim Klimenko <52748143+Infron@users.noreply.github.com>
2022-02-15 03:17:35 +0300

    Add scala 2

--- tasks/scala/2/src/main/scala/funsets/Main.scala
+++ tasks/scala/2/src/main/scala/funsets/Main.scala
@@ -3,4 +3,9 @@ package funsets
 object Main extends App {
   import FunSets._
   println(contains(singletonSet(1), 1))
+  println(contains(singletonSet(2), 1))
+  printSet(union(singletonSet(2), singletonSet(3)))
+  printSet(union(union(singletonSet(1), singletonSet(2)), union(singletonSet(2), singletonSet(3))))
+  printSet(intersect(union(singletonSet(1), singletonSet(2)), union(singletonSet(2), singletonSet(3))))
+  printSet(diff(union(singletonSet(1), singletonSet(2)), union(singletonSet(2), singletonSet(3))))
 }

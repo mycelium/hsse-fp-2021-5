172bc3be8a48091bf0b94675849b3282ce9a8abe
Maxim Lebedev <>
2022-02-27 01:09:55 +0300

    added prolog lab 2

--- tasks/prolog/2_truth_table.pl
+++ tasks/prolog/2_truth_table.pl
@@ -10,3 +10,48 @@
 % true fail true
 % fail true fail
 % fail fail fail
+
+and(A,B):- A, B.
+or(A,B):- A; B.
+xor(A,B):- A, not(B); not(A), B.
+equ(A,B):- A = B.
+
+operand(true).
+operand(fail).
+
+truth_table(A,B,Expression):-
+	operand(A), operand(B),
+	write(A), write(" "), write(B), write(" "),
+	(Expression -> write(true); write(fail)),
+	write("\n"),
+	fail.
+
+/*
+	?- truth_table(X, Y, and(X, Y)).
+	true true true
+	true fail fail
+	fail true fail
+	fail fail fail
+	false.
+
+	?- truth_table(X, Y, or(X, Y)).
+	true true true
+	true fail true
+	fail true true
+	fail fail fail
+	false.
+
+	?- truth_table(X, Y, xor(X, Y)).
+	true true fail
+	true fail true
+	fail true true
+	fail fail fail
+	false.
+
+	?- truth_table(X, Y, equ(X, Y)).
+	true true true
+	true fail fail
+	fail true fail
+	fail fail true
+	false.
+*/

3bc3bb950df0c25f5ef45d4d7c7eddae8b273858
Maxim Lebedev <>
2022-02-28 00:48:15 +0300

    added prolog lab 3

--- tasks/prolog/3_quicksort.pl
+++ tasks/prolog/3_quicksort.pl
@@ -1,1 +1,22 @@
 % Определить предикат qsort(L, K) который для заданного списка целых чисел возвращает отсортированный 
+
+qsort([],[]).
+
+qsort([X|Tail], Sorted) :-
+    split(X, Tail, Small, Big),
+	qsort(Small, SortedSmall),
+	qsort(Big, SortedBig),
+	append(SortedSmall, [X|SortedBig], Sorted).
+
+split(_, [], [], []).
+split(X, [Y|Tail], [Y|Small], Big) :-
+	X > Y,
+	split(X, Tail, Small, Big).
+split(X, [Y|Tail], Small, [Y|Big]) :-
+	split(X, Tail, Small, Big).
+
+% ? - qsort([10, 5, 0, 6, 8, -13], Sorted)
+% K = [-13, 0, 5, 6, 8, 10]
+
+% ? - qsort([10, 5, 0, 6, 8, -13], [-13, 0, 5, 6, 8, 10])
+% K = true
